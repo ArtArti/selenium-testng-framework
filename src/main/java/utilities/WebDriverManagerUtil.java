1d827d77ad057b475c5112f777719d27f0f606e5
Arti <singharti7648@gmail.com>
2025-06-03 12:37:51 +0530

    output file generated

--- src/main/java/utilities/WebDriverManagerUtil.java
+++ src/main/java/utilities/WebDriverManagerUtil.java
@@ -3,11 +3,12 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+
 
 public class WebDriverManagerUtil {
 
-    // Thread-safe WebDriver for parallel tests
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver getDriver(String browser) {
@@ -15,15 +16,26 @@ public class WebDriverManagerUtil {
             switch (browser.toLowerCase()) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver.set(new ChromeDriver());
+                    ChromeOptions chromeOptions = new ChromeOptions();
+
+                    chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+
+                    driver.set(new ChromeDriver(chromeOptions));
                     break;
+
                 case "edge":
                     WebDriverManager.edgedriver().setup();
                     driver.set(new EdgeDriver());
                     break;
+
                 default:
                     throw new IllegalArgumentException("Unsupported browser: " + browser);
             }
+
             driver.get().manage().window().maximize();
         }
         return driver.get();
@@ -33,11 +45,10 @@ public class WebDriverManagerUtil {
         return driver.get();
     }
 
-
     public static void quitDriver() {
         if (driver.get() != null) {
             driver.get().quit();
-            driver.remove(); // Very important to remove the thread-local instance
+            driver.remove();
         }
     }
 }
